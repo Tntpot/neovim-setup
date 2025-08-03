f6e92ce612fe2ea9fa05cfac99894095947468c7
Tntpot <daniel_ferguson@hotmail.co.uk>
2025-08-03 10:12:58 +0100

    Harpoon - next/prev fix

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-p>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
     end
 }
