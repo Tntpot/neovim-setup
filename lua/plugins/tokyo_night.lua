3b9e599c61b6a61ec96d1a08c904dc265c0add7e
Tntpot <daniel_ferguson@hotmail.co.uk>
2025-09-03 20:45:44 +0100

    Update: Tokyo Night conditional transparency & format

--- lua/plugins/tokyo_night.lua
+++ lua/plugins/tokyo_night.lua
@@ -1,15 +1,21 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("tokyonight").setup({
-			terminal_colors = true,
-			on_colors = function(colors)
-				colors.comment = colors.orange
-			end,
-		})
-		-- load the colorscheme here
-		vim.cmd.colorscheme("tokyonight-storm")
-	end,
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        local transparent = true
+        local getOS = require("config.getOS")
+        if getOS.getName() == "Windows" then
+            transparent = false
+        end
+        require("tokyonight").setup({
+            transparent = transparent,
+            terminal_colors = true,
+            on_colors = function(colors)
+                colors.comment = colors.orange
+            end,
+        })
+        -- load the colorscheme here
+        vim.cmd.colorscheme("tokyonight-storm")
+    end,
 }
