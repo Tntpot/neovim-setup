d49362450f85d4bad523c73aeeb9f9a1debfe3ae
Tntpot <daniel_ferguson@hotmail.co.uk>
2025-10-13 08:23:09 +0100

    feat: Lualine event 'ColorScheme' added & format

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,20 +1,21 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				icons_enabled = false,
-				-- theme = "tokyonight",
+    "nvim-lualine/lualine.nvim",
+    event = "ColorScheme",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                icons_enabled = false,
+                -- theme = "tokyonight",
                 theme = "auto",
-				component_separators = "|",
-				section_separators = "",
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
-			},
-		})
-	end,
+                component_separators = "|",
+                section_separators = "",
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = { "filename" },
+            },
+        })
+    end,
 }
